02368304033aa5fc147f159fd60c4a9f35bcb871
grupostartin <contatostartin@gmail.com>
2025-11-24 17:23:38 -0300

    empresa

--- src/components/sections/FooterSection.tsx
+++ src/components/sections/FooterSection.tsx
@@ -86,14 +86,14 @@ export const FooterSection = () => {
               className="rounded-2xl overflow-hidden shadow-2xl border-4 border-white/20 h-[300px] w-full bg-white"
             >
               <iframe 
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d240099.3624343523!2d-44.10413656279286!3d-19.90266149238534!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa690cacacf2c33%3A0x5b35795e3ad23997!2sBelo%20Horizonte%20-%20MG!5e0!3m2!1spt-BR!2sbr!4v1650000000000!5m2!1spt-BR!2sbr" 
+                src="https://maps.google.com/maps?q=R.%20Tapira%2C%201105%20-%20Renascen%C3%A7a%2C%20Belo%20Horizonte%20-%20MG%2C%2031130-550&t=&z=15&ie=UTF8&iwloc=&output=embed"
                 width="100%" 
                 height="100%" 
                 style={{ border: 0 }} 
                 allowFullScreen 
                 loading="lazy" 
                 referrerPolicy="no-referrer-when-downgrade"
-                title="Mapa de Belo Horizonte"
+                title="Localização Refrizzari"
               />
             </motion.div>
 
